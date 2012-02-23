2e9537f3ac1630f9de9e9fec6b215378173e5855
Krzesimir Nowak <krnowak@openismus.com>
2012-02-23 16:46:11 +0100

    Refactored ProxyCallback.
    Main aim of refactoring was to introduce different success signal
    types. Sometimes we want to know not only whether DBus call succeeded,
    but also what values it returned. So now ProxyCallback{0,1,2,3} are
    introduced. The last three ones are templates, which take return
    types as parameters.

--- src/dbus/server/dbus-proxy.h
+++ src/dbus/server/dbus-proxy.h
@@ -34,8 +34,34 @@
 #include <syncevo/declarations.h>
 SE_BEGIN_CXX
 
-typedef boost::signals2::signal<void ()> DBusSuccessSignal_t;
-typedef boost::signals2::signal<void (const std::string &)> DBusFailureSignal_t;
+typedef boost::signals2::signal<void (const std::string &)> DBusFailureSignalType;
+
+struct ProxyCallbackTraits0
+{
+    typedef boost::signals2::signal<void ()> SuccessSignalType;
+    typedef GDBusCXX::Result0 ResultType;
+};
+
+template <class A1>
+struct ProxyCallbackTraits1
+{
+    typedef typename boost::signals2::signal<void (const A1 &)> SuccessSignalType;
+    typedef typename GDBusCXX::Result1<A1> ResultType;
+};
+
+template <class A1, class A2>
+struct ProxyCallbackTraits2
+{
+    typedef typename boost::signals2::signal<void (const A1 &, const A2 &)> SuccessSignalType;
+    typedef typename GDBusCXX::Result2<A1, A2> ResultType;
+};
+
+template <class A1, class A2, class A3>
+struct ProxyCallbackTraits3
+{
+    typedef typename boost::signals2::signal<void (const A1 &, const A2 &, const A3 &)> SuccessSignalType;
+    typedef typename GDBusCXX::Result3<A1, A2, A3> ResultType;
+};
 
 /**
  * Use this class in syncevo-dbus-server as callback for asynchronous
@@ -55,57 +81,137 @@ typedef boost::signals2::signal<void (const std::string &)> DBusFailureSignal_t;
  * the GDBus C++ bindings, but because they happen inside the main
  * event loop, they cannot be propagated to the upper layers.
  *
- * @param R     some kind of GDBusCXX::Result0/1/2/3
+ * @param T     traits class (one of ProxyCallbackTraits{0,1,2,3})
  */
-template <class R> class ProxyCallback
+template <class T> class ProxyCallbackBase
 {
  public:
-    ProxyCallback(const boost::shared_ptr<R> &result) :
-        m_success(new DBusSuccessSignal_t),
-        m_failure(new DBusFailureSignal_t),
+    typedef typename T::SuccessSignalType SuccessSignalType;
+    typedef DBusFailureSignalType FailureSignalType;
+    typedef typename T::ResultType ResultType;
+
+    ProxyCallbackBase(const boost::shared_ptr<ResultType> &result) :
+        m_success(new SuccessSignalType),
+        m_failure(new FailureSignalType),
         m_result(result)
     {}
-
-    void operator () (const std::string &error);
 
     /**
      * Triggered after a successful method call was reported back to
      * the original caller.
      */
-    boost::shared_ptr< DBusSuccessSignal_t > m_success;
+    boost::shared_ptr<SuccessSignalType> m_success;
 
     /**
      * Triggered after a method call failure was reported back to the
      * original caller.
      */
-    boost::shared_ptr< DBusFailureSignal_t > m_failure;
+    boost::shared_ptr<FailureSignalType> m_failure;
 
- private:
-    boost::shared_ptr<R> m_result;
+ protected:
+    boost::shared_ptr<ResultType> m_result;
 };
 
-template <class R> void ProxyCallback<R>::operator () (const std::string &error)
+class ProxyCallback0 : public ProxyCallbackBase<ProxyCallbackTraits0>
 {
-    if (error.empty()) {
-        // TODO: split error into bus name + description and relay exactly that
-        // (see Chris' code for that)
-        m_result->failed(GDBusCXX::dbus_error("org.syncevolution.gdbuscxx.Exception",
-                                             error));
-        (*m_failure)(error);
-    } else {
-        m_result->done();
-        (*m_success)();
-    }
-}
+ public:
+    ProxyCallback0(const boost::shared_ptr<GDBusCXX::Result0> &result) :
+        ProxyCallbackBase<ProxyCallbackTraits0> (result)
+    {}
 
-/**
- * utility method for creating a callback of the right type
- */
-template <class R> class ProxyCallback<R> MakeProxyCallback(const boost::shared_ptr<R> &result)
+    void operator () (const std::string &error)
+    {
+        if (!error.empty()) {
+            // TODO: split error into bus name + description and relay exactly that
+            // (see Chris' code for that)
+            m_result->failed(GDBusCXX::dbus_error("org.syncevolution.gdbuscxx.Exception",
+                                                  error));
+            (*m_failure)(error);
+        } else {
+            m_result->done();
+            (*m_success)();
+        }
+    }
+};
+
+template <class A1>
+class ProxyCallback1 : public ProxyCallbackBase<ProxyCallbackTraits1<A1> >
 {
-    return ProxyCallback<R>(result);
-}
+ public:
+    ProxyCallback1(const boost::shared_ptr<GDBusCXX::Result1<A1> > &result) :
+        ProxyCallbackBase<ProxyCallbackTraits1<A1> >(result)
+    {}
+
+    void operator () (const A1 &a1,
+                      const std::string &error)
+    {
+        // using this-> notation, because otherwise compiler says
+        // that m_{failure,success,result} is undefined.
+        if (!error.empty()) {
+            // TODO: split error into bus name + description and relay exactly that
+            // (see Chris' code for that)
+            this->m_result->failed(GDBusCXX::dbus_error("org.syncevolution.gdbuscxx.Exception",
+                                                        error));
+            (*(this->m_failure))(error);
+        } else {
+            this->m_result->done(a1);
+            (*(this->m_success))(a1);
+        }
+    }
+};
+
+template <class A1, class A2>
+class ProxyCallback2 : public ProxyCallbackBase<ProxyCallbackTraits2<A1, A2> >
+{
+ public:
+    ProxyCallback2(const boost::shared_ptr<GDBusCXX::Result2<A1, A2> > &result) :
+        ProxyCallbackBase<ProxyCallbackTraits2<A1, A2> >(result)
+    {}
+
+    void operator () (const A1 &a1, const A2 &a2,
+                      const std::string &error)
+    {
+        // using this-> notation, because otherwise compiler says
+        // that m_{failure,success,result} is undefined.
+        if (!error.empty()) {
+            // TODO: split error into bus name + description and relay exactly that
+            // (see Chris' code for that)
+            this->m_result->failed(GDBusCXX::dbus_error("org.syncevolution.gdbuscxx.Exception",
+                                                        error));
+            (*(this->m_failure))(error);
+        } else {
+            this->m_result->done(a1, a2);
+            (*(this->m_success))(a1, a2);
+        }
+    }
+};
+
+template <class A1, class A2, class A3>
+  class ProxyCallback3 : public ProxyCallbackBase<ProxyCallbackTraits3<A1, A2, A3> >
+{
+ public:
+    ProxyCallback3(const boost::shared_ptr<GDBusCXX::Result3<A1, A2, A3> > &result) :
+        ProxyCallbackBase<ProxyCallbackTraits3<A1, A2, A3> >(result)
+    {}
+
+    void operator () (const A1 &a1, const A2 &a2, const A3 &a3,
+                      const std::string &error)
+    {
+        // using this-> notation, because otherwise compiler says
+        // that m_{failure,success,result} is undefined.
+        if (!error.empty()) {
+            // TODO: split error into bus name + description and relay exactly that
+            // (see Chris' code for that)
+            this->m_result->failed(GDBusCXX::dbus_error("org.syncevolution.gdbuscxx.Exception",
+                                                        error));
+            (*(this->m_failure))(error);
+        } else {
+            this->m_result->done(a1, a2, a3);
+            (*(this->m_success))(a1, a2, a3);
+        }
+    }
+};
 
 SE_END_CXX
 
-#endif // RESOURCE_H
+#endif // DBUS_PROXY_H
