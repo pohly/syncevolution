ea16d760117c3d9f60a9c34968e0df94b6ea1acd
Ove Kåven <ovek@arcticnet.no>
2012-08-05 04:07:37 +0200

    Various packaging and build system patches for the Maemo 5 port.

--- autogen-maemo.sh
+++ autogen-maemo.sh
@@ -3,25 +3,31 @@
 set -e
 
 # wipe out temporary autotools files, necessary
-# when switching between distros
-rm -rf aclocal.m4 m4 autom4te.cache config.guess config.sub config.h.in configure depcomp install-sh ltmain.sh missing 
+# when switching between distros and SyncEvolution releases
+rm -rf aclocal.m4 m4 autom4te.cache config.guess config.sub config.h.in configure depcomp install-sh ltmain.sh missing configure.in src/Makefile.am
 
 # intltoolize fails to copy its macros unless m4 exits
 mkdir m4
 
-sh ./gen-autotools.sh
+#env GEN_AUTOTOOLS_SET_VERSION=1 sh ./gen-autotools.sh
 
-libtoolize -c
 glib-gettextize --force --copy
 intltoolize --force --copy --automake
-aclocal-1.9 -I m4 -I m4-repo
-autoheader
-automake-1.9 -a -c -Wno-portability
-autoconf
+ACLOCAL=aclocal-1.10 \
+AUTOMAKE=automake-1.10 \
+autoreconf2.50 -vifW all -W no-portability -W no-obsolete
+
+#libtoolize -c
+#glib-gettextize --force --copy
+#intltoolize --force --copy --automake
+#aclocal-1.10 -I m4 -I m4-repo
+#autoheader
+#automake-1.10 -a -c -Wno-portability
+#autoconf
 
 # This hack is required for the autotools on Debian Etch.
 # Without it, configure expects a po/Makefile where
 # only po/Makefile.in is available. This patch fixes
 # configure so that it uses po/Makefile.in, like more
 # recent macros do.
-perl -pi -e 's;test ! -f "po/Makefile";test ! -f "po/Makefile.in";; s;mv "po/Makefile" "po/Makefile.tmp";cp "po/Makefile.in" "po/Makefile.tmp";;' configure
+#perl -pi -e 's;test ! -f "po/Makefile";test ! -f "po/Makefile.in";; s;mv "po/Makefile" "po/Makefile.tmp";cp "po/Makefile.in" "po/Makefile.tmp";;' configure
