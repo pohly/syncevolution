3691f52ccfa0e6a2f740bf635e95b9e4c9046e44
Patrick Ohly <patrick.ohly@intel.com>
2012-09-04 14:57:41 +0200

    folks: initial set of classes for viewing, sorting, searching
    The classes on top of libfolks are  designed such that they can
    be tested without involving libfolks. Right now the goal is only
    to get the API right; most of the actual functionality is missing.
    
    The terminology (added/modified/removed) is aligned with libebook
    terminology and the PIM Manager API description.

--- src/dbus/server/pim/folks.cpp
+++ src/dbus/server/pim/folks.cpp
@@ -18,32 +18,216 @@
  */
 
 #include <config.h>
-
-#include <folks/folks.h>
-
+#include "folks.h"
 #include <boost/bind.hpp>
-
-#include <syncevo/GLibSupport.h>
-#include <syncevo/GeeSupport.h>
-#include <syncevo/GValueSupport.h>
 #include "test.h"
 
-SE_GOBJECT_TYPE(FolksIndividualAggregator)
-SE_GOBJECT_TYPE(FolksIndividual)
-SE_GOBJECT_TYPE(FolksEmailFieldDetails)
 
 #include <syncevo/declarations.h>
 SE_BEGIN_CXX
 
+bool IndividualCompare::compare(const Criteria_t &a, const Criteria_t &b) const
+{
+    Criteria_t::const_iterator ita = a.begin(),
+        itb = b.begin();
+
+    while (itb != b.end()) {
+        if (ita == a.end()) {
+            // a is shorter
+            return true;
+        }
+        int cmp = ita->compare(*itb);
+        if (cmp < 0) {
+            // String comparison shows that a is less than b.
+            return true;
+        } else if (cmp > 0) {
+            // Is greater, so definitely not less => don't compare
+            // rest of the criteria.
+            return false;
+        } else {
+            // Equal, continue comparing.
+            ++ita;
+            ++itb;
+        }
+    }
+
+    // a is not less b
+    return false;
+}
+
+boost::shared_ptr<FullView> FullView::create()
+{
+    boost::shared_ptr<FullView> view(new FullView);
+    view->m_self = view;
+    return view;
+}
+
+void FullView::addIndividual(FolksIndividual *individual)
+{
+    // TODO
+}
+void FullView::removeIndividual(FolksIndividual *individual)
+{
+    // TODO
+}
+void FullView::setCompare(const boost::shared_ptr<IndividualCompare> &compare)
+{
+    // TODO
+}
+
+boost::shared_ptr<FilteredView> FilteredView::create(const boost::shared_ptr<IndividualView> &parent,
+                                                     const boost::shared_ptr<IndividualFilter> &filter)
+{
+    boost::shared_ptr<FilteredView> view(new FilteredView);
+    view->m_self = view;
+    view->m_parent = parent;
+    // TODO
+    return view;
+}
+void FilteredView::start()
+{
+    // TODO
+}
+void FilteredView::addIndividual(int parentIndex, FolksIndividual *individual)
+{
+    // TODO
+}
+void FilteredView::removeIndividual(int parentIndex, FolksIndividual *individual)
+{
+    // TODO
+}
+void FilteredView::changeIndividual(int parentIndex, FolksIndividual *individual)
+{
+    // TODO
+}
+
+boost::shared_ptr<IndividualAggregator> IndividualAggregator::create()
+{
+    boost::shared_ptr<IndividualAggregator> aggregator(new IndividualAggregator);
+    aggregator->m_self = aggregator;
+    aggregator->m_folks =
+        FolksIndividualAggregatorCXX::steal(folks_individual_aggregator_new());
+    boost::shared_ptr<FullView> view(FullView::create());
+    aggregator->m_view = view;
+    aggregator->m_folks.connectSignal<void (FolksIndividualAggregator *,
+                                            GeeSet *added,
+                                            GeeSet *removed,
+                                            gchar *message,
+                                            FolksPersona *actor,
+                                            FolksGroupDetailsChangeReason)>("individuals-changed",
+                                                                            boost::bind(&IndividualAggregator::individualsChanged,
+                                                                                        aggregator.get(),
+                                                                                        _2, _3, _4));
+    return aggregator;
+}
+
+void IndividualAggregator::individualsChanged(GeeSet *added,
+                                              GeeSet *removed,
+                                              gchar *message) throw ()
+{
+    // TODO
+}
+
+/**
+ * Generic error callback. There really isn't much that can be done if
+ * libfolks fails, except for logging the problem.
+ */
+static void logResult(const GError *gerror, const char *operation)
+{
+    if (gerror) {
+        SE_LOG_ERROR(NULL, NULL, "%s: %s", operation, gerror->message);
+    } else {
+        SE_LOG_DEBUG(NULL, NULL, "%s: done", operation);
+    }
+}
+
+void IndividualAggregator::start()
+{
+    SYNCEVO_GLIB_CALL_ASYNC(folks_individual_aggregator_prepare,
+                            boost::bind(logResult, _1,
+                                        "folks_individual_aggregator_prepare"),
+                            getFolks());
+}
+
 #ifdef ENABLE_UNIT_TESTS
+
+class CompareFormattedName : public IndividualCompare {
+    bool m_reversed;
+    bool m_firstLast;
+
+public:
+    CompareFormattedName(bool reversed = false, bool firstLast = false) :
+        m_reversed(reversed),
+        m_firstLast(firstLast)
+    {
+    }
+
+    virtual void createCriteria(FolksIndividual *individual, Criteria_t &criteria) const {
+        FolksStructuredName *fn =
+            folks_name_details_get_structured_name(FOLKS_NAME_DETAILS(individual));
+        if (fn) {
+            const char *family = folks_structured_name_get_family_name(fn);
+            const char *given = folks_structured_name_get_given_name(fn);
+            if (m_firstLast) {
+                criteria.push_back(given ? given : "");
+                criteria.push_back(family ? family : "");
+            } else {
+                criteria.push_back(family ? family : "");
+                criteria.push_back(given ? given : "");
+            }
+        }
+    }
+
+    virtual bool compare(const Criteria_t &a, const Criteria_t &b) const {
+        return m_reversed ?
+            IndividualCompare::compare(b, a) :
+            IndividualCompare::compare(a, b);
+    }
+};
 
 class FolksTest : public CppUnit::TestFixture {
     CPPUNIT_TEST_SUITE(FolksTest);
     CPPUNIT_TEST(open);
     CPPUNIT_TEST(asyncError);
     CPPUNIT_TEST(gvalue);
+    CPPUNIT_TEST(fullview);
+    CPPUNIT_TEST(filter);
+    CPPUNIT_TEST(aggregate);
     CPPUNIT_TEST_SUITE_END();
 
+    FolksIndividualCXX m_contactA,
+        m_contactB,
+        m_contactC;
+
+public:
+    void setUp()
+    {
+        FolksIndividualCXX contact;
+        FolksStructuredName *fn;
+
+        contact = FolksIndividualCXX::steal(folks_individual_new(NULL));
+        folks_name_details_set_full_name(FOLKS_NAME_DETAILS(contact.get()), "Abraham Zzz");
+        fn = folks_name_details_get_structured_name(FOLKS_NAME_DETAILS(contact.get()));
+        folks_structured_name_set_family_name(fn, "Zzz");
+        folks_structured_name_set_given_name(fn, "Abraham");
+        m_contactA = contact;
+
+        contact = FolksIndividualCXX::steal(folks_individual_new(NULL));
+        folks_name_details_set_full_name(FOLKS_NAME_DETAILS(contact.get()), "Benjamin Yyy");
+        fn = folks_name_details_get_structured_name(FOLKS_NAME_DETAILS(contact.get()));
+        folks_structured_name_set_family_name(fn, "Yyy");
+        folks_structured_name_set_given_name(fn, "Benjamin");
+        m_contactB = contact;
+
+        contact = FolksIndividualCXX::steal(folks_individual_new(NULL));
+        folks_name_details_set_full_name(FOLKS_NAME_DETAILS(contact.get()), "Charly Xxx");
+        fn = folks_name_details_get_structured_name(FOLKS_NAME_DETAILS(contact.get()));
+        folks_structured_name_set_family_name(fn, "Xxx");
+        folks_structured_name_set_given_name(fn, "Charly");
+        m_contactC = contact;
+    }
+
+private:
     static void asyncCB(const GError *gerror, const char *func, bool &failed, bool &done) {
         done = true;
         if (gerror) {
@@ -183,6 +367,161 @@ class FolksTest : public CppUnit::TestFixture {
         }
         // Invalid parameters are not reported!
         CPPUNIT_ASSERT(!failed);
+
+        // using simpler macro
+        GErrorCXX gerror;
+        SYNCEVO_GLIB_CALL_SYNC(NULL,
+                               gerror,
+                               folks_individual_aggregator_remove_individual,
+                               NULL, NULL);
+        // Invalid parameters are not reported!
+        CPPUNIT_ASSERT(!gerror);
+    }
+
+    static void individualSignal(std::ostringstream &out,
+                                 const char *action,
+                                 int index,
+                                 FolksIndividual *individual) {
+        out << action << ": " << index << " " <<
+            folks_name_details_get_full_name(FOLKS_NAME_DETAILS(individual)) <<
+            std::endl;
+    }
+
+    static void monitorView(IndividualView &view, std::ostringstream &out) {
+        view.m_addedSignal.connect(boost::bind(individualSignal, boost::ref(out), "added", _1, _2));
+        view.m_removedSignal.connect(boost::bind(individualSignal, boost::ref(out), "removed", _1, _2));
+        view.m_modifiedSignal.connect(boost::bind(individualSignal, boost::ref(out), "modified", _1, _2));
+    }
+
+    void fullview() {
+        boost::shared_ptr<FullView> view(FullView::create());
+        std::ostringstream out;
+        monitorView(*view, out);
+
+        // add and remove
+        view->addIndividual(m_contactA.get());
+        CPPUNIT_ASSERT_EQUAL(std::string("added: 0 Abraham Zzz\n"), out.str());
+        CPPUNIT_ASSERT_EQUAL(1, view->size());
+        out.str("");
+        view->removeIndividual(m_contactA);
+        CPPUNIT_ASSERT_EQUAL(std::string("removed: 0 Abraham Zzz\n"), out.str());
+        CPPUNIT_ASSERT_EQUAL(0, view->size());
+        out.str("");
+
+        // add three, in inverse order
+        view->addIndividual(m_contactC.get());
+        CPPUNIT_ASSERT_EQUAL(std::string("added: 0 Charly Xxx\n"), out.str());
+        CPPUNIT_ASSERT_EQUAL(1, view->size());
+        out.str("");
+        view->addIndividual(m_contactB.get());
+        CPPUNIT_ASSERT_EQUAL(std::string("added: 0 Benjamin Yyy\n"), out.str());
+        CPPUNIT_ASSERT_EQUAL(2, view->size());
+        out.str("");
+        view->addIndividual(m_contactA.get());
+        CPPUNIT_ASSERT_EQUAL(std::string("added: 0 Abraham Zzz\n"), out.str());
+        CPPUNIT_ASSERT_EQUAL(3, view->size());
+        out.str("");
+
+        // change sorting: last,first inverse
+        boost::shared_ptr<IndividualCompare> compare(new CompareFormattedName(true, false));
+        view->setCompare(compare);
+        // Exact sequence of events is not specified. The current
+        // implementation prefers to announce changes at the end of
+        // the array first.
+        CPPUNIT_ASSERT_EQUAL(std::string("removed: 2 Charly Xxx\n"
+                                         "removed: 1 Benjamin Yyy\n"
+                                         "removed: 0 Abraham Zzz\n"
+                                         "added: 0 Abraham Zzz\n"
+                                         "added: 0 Benjamin Yyy\n"
+                                         "added: 0 Charly Xxx\n"),
+                             out.str());
+        CPPUNIT_ASSERT_EQUAL(3, view->size());
+
+        // TODO: insert sorted
+
+    }
+
+    void filter() {
+        class FilterFullName : public IndividualFilter {
+            std::string m_name;
+            bool m_negated;
+        public:
+            FilterFullName(const std::string &name,
+                           bool negated = false) :
+                m_name(name),
+                m_negated(negated)
+            {
+            }
+
+            virtual bool matches(FolksIndividual *individual) const {
+                return (m_name == folks_name_details_get_full_name(FOLKS_NAME_DETAILS(individual)))
+                    ^ m_negated;
+            }
+        };
+
+        // start with full view
+        boost::shared_ptr<FullView> view(FullView::create());
+        view->addIndividual(m_contactA.get());
+        view->addIndividual(m_contactB.get());
+        view->addIndividual(m_contactC.get());
+        CPPUNIT_ASSERT_EQUAL(3, view->size());
+        boost::shared_ptr<IndividualFilter> filterFullName(static_cast<IndividualFilter *>(new FilterFullName("Benjamin Yyy")));
+        boost::shared_ptr<FilteredView> filter(FilteredView::create(view,
+                                                                    filterFullName));
+        std::ostringstream out;
+        monitorView(*filter, out);
+        filter->start();
+        CPPUNIT_ASSERT_EQUAL(std::string("added: 0 Benjamin Yyy"), out.str());
+        CPPUNIT_ASSERT_EQUAL(1, filter->size());
+
+        // remove all individuals
+        out.str("");
+        view->removeIndividual(m_contactA.get());
+        CPPUNIT_ASSERT_EQUAL(std::string(""), out.str());
+        CPPUNIT_ASSERT_EQUAL(2, view->size());
+        CPPUNIT_ASSERT_EQUAL(1, filter->size());
+        view->removeIndividual(m_contactB.get());
+        CPPUNIT_ASSERT_EQUAL(std::string("removed: 0 Benjamin Yyy"), out.str());
+        CPPUNIT_ASSERT_EQUAL(1, view->size());
+        CPPUNIT_ASSERT_EQUAL(0, filter->size());
+        out.str("");
+        view->removeIndividual(m_contactC.get());
+        CPPUNIT_ASSERT_EQUAL(std::string(""), out.str());
+        CPPUNIT_ASSERT_EQUAL(0, view->size());
+        CPPUNIT_ASSERT_EQUAL(0, filter->size());
+
+        // add again
+        view->addIndividual(m_contactA.get());
+        CPPUNIT_ASSERT_EQUAL(std::string(""), out.str());
+        CPPUNIT_ASSERT_EQUAL(1, view->size());
+        CPPUNIT_ASSERT_EQUAL(0, filter->size());
+        view->addIndividual(m_contactB.get());
+        CPPUNIT_ASSERT_EQUAL(std::string("added: 0 Benjamin Yyy"), out.str());
+        CPPUNIT_ASSERT_EQUAL(2, view->size());
+        CPPUNIT_ASSERT_EQUAL(1, filter->size());
+        out.str("");
+        view->addIndividual(m_contactC.get());
+        CPPUNIT_ASSERT_EQUAL(std::string(""), out.str());
+        CPPUNIT_ASSERT_EQUAL(3, view->size());
+        CPPUNIT_ASSERT_EQUAL(1, filter->size());
+
+        // TODO: same as above, with inverted filter
+
+        // TODO: same as above, with inverted filter and reversed order
+
+        // TODO: change sorting while we have a filter view open
+    }
+
+    void aggregate() {
+        // TODO: run with well-defined set of databases and database content
+        boost::shared_ptr<IndividualAggregator> aggregator(IndividualAggregator::create());
+        boost::shared_ptr<IndividualView> view(aggregator->getMainView());
+        std::ostringstream out;
+        monitorView(*view, out);
+        aggregator->start();
+        while (!folks_individual_aggregator_get_is_quiescent(aggregator->getFolks())) {
+            g_main_context_iteration(NULL, true);
+        }
     }
 };
 
--- src/dbus/server/pim/folks.h
+++ src/dbus/server/pim/folks.h
@@ -17,11 +17,274 @@
  * 02110-1301  USA
  */
 
+/**
+ * The classes in this file implement sorting, searching and
+ * configuring of the unified address book based on libfolks.
+ *
+ * This is pure C++ code. The D-Bus IPC binding for it is
+ * implemented separately in pim-manager.h/cpp.
+ */
+
 #ifndef INCL_SYNCEVO_DBUS_SERVER_IVI_FOLKS
 #define INCL_SYNCEVO_DBUS_SERVER_IVI_FOLKS
 
+#include <folks/folks.h>
+
+#include <syncevo/GLibSupport.h>
+#include <syncevo/GeeSupport.h>
+#include <syncevo/GValueSupport.h>
+
+#include <boost/shared_ptr.hpp>
+#include <boost/signals2.hpp>
+
+SE_GOBJECT_TYPE(FolksIndividualAggregator)
+SE_GOBJECT_TYPE(FolksIndividual)
+SE_GOBJECT_TYPE(FolksEmailFieldDetails)
+
 #include <syncevo/declarations.h>
 SE_BEGIN_CXX
+
+/**
+ * Abstract interface for comparing two FolksIndividual instances.
+ * The properties of a folks individual may change at any time.
+ * Therefore the key properties which determine the sort order
+ * must be copied from the individual. They will be updated when
+ * the individual changes.
+ *
+ * The other advantage is that complex, derived keys only need
+ * to be computed once instead of each time during a comparison.
+ * For example, boost::locale::collator::transform() can be used
+ * to generate the keys.
+ */
+class IndividualCompare
+{
+ public:
+    virtual ~IndividualCompare() {}
+
+    typedef std::vector<std::string> Criteria_t;
+
+    /**
+     * Calculate an ordered set of criteria for comparing
+     * individuals. The default comparison will start with the initial
+     * criteria and move on until a difference is found.
+     *
+     * This is necessary because a single string of "Doe, John" is
+     * treated differently in a collation than the pair of strings
+     * "Doe" and "John".
+     *
+     * @param individual    the individual for which sort keys are to be created
+     * @retval criteria     cleared before the call, filled afterwards
+     */
+    virtual void createCriteria(FolksIndividual *individual, Criteria_t &criteria) const = 0;
+
+    /**
+     * Partial sort order: true if a smaller than b.
+     *
+     * Default implementation uses normal std::string::compare().
+     */
+    virtual bool compare(const Criteria_t &a, const Criteria_t &b) const;
+};
+
+/**
+ * A FolksIndividual plus its sort criteria.
+ */
+struct IndividualData
+{
+    FolksIndividualCXX m_individual;
+    IndividualCompare::Criteria_t m_criteria;
+};
+
+/**
+ * Abstract interface for filtering (aka searching) FolksIndividual
+ * instances.
+ */
+class IndividualFilter
+{
+ public:
+    virtual ~IndividualFilter() {}
+
+    /** true if the contact matches the filter */
+    virtual bool matches(FolksIndividual *individual) const = 0;
+};
+
+class IndividualAggregator;
+
+/**
+ * A view on a sorted list of individuals. Entries are numbered from
+ * #0 to #n - 1, where n is the number of entries. Change
+ * notifications are based upon those numbers and will be triggered
+ * immediately.
+ */
+class IndividualView
+{
+ public:
+    typedef boost::signals2::signal<void (int, FolksIndividual *)> ChangeSignal_t;
+
+    /**
+     * A new FolksIndividual was added at a specific index. This
+     * increased the index of all individuals it was inserted in front
+     * off by one.
+     */
+    ChangeSignal_t m_addedSignal;
+
+    /**
+     * A FolksIndividual was removed at a specific index. This
+     * increased the index of all individuals after it by one.
+     */
+    ChangeSignal_t m_removedSignal;
+
+    /**
+     * A FolksIndividual was modified at a specific index, without
+     * affecting its position in the view. If changing a FolksIndividual
+     * affects its position, m_removedSignal followed by m_addedSignal
+     * will be emitted.
+     */
+    ChangeSignal_t m_modifiedSignal;
+
+    /** current number of entries */
+    virtual int size() const = 0;
+};
+
+/**
+ * The view which takes input directly from IndividualAggregator
+ * and maintains a sorted set of contacts as result.
+ */
+class FullView : public IndividualView
+{
+    boost::weak_ptr<FullView> m_self;
+
+    /**
+     * Sorted vector. Sort order is maintained by this class.
+     */
+    std::vector<IndividualData> m_entries;
+
+    /**
+     * The sort object to be used.
+     */
+    boost::shared_ptr<IndividualCompare> m_compare;
+
+    FullView() {}
+
+ public:
+    static boost::shared_ptr<FullView> create();
+
+    /**
+     * Add a FolksIndividual. Starts monitoring it for changes.
+     */
+    void addIndividual(FolksIndividual *individual);
+
+    /**
+     * Remove a FolksIndividual.
+     */
+    void removeIndividual(FolksIndividual *individual);
+
+    /**
+     * Set new sort method. Reorders current set of entries on the
+     * fly. Default is lexicographical comparison of the single-string
+     * full name.
+     */
+    void setCompare(const boost::shared_ptr<IndividualCompare> &compare);
+
+    // from IndividualView
+    virtual int size() const { return (int)m_entries.size(); }
+};
+
+/**
+ * A subset of some other view. Takes input from that view and thus
+ * can rely on individuals being sorted by their index number in the
+ * other view.
+ */
+class FilteredView : public IndividualView
+{
+    boost::weak_ptr<FilteredView> m_self;
+    boost::shared_ptr<IndividualFilter> m_filter;
+    boost::shared_ptr<IndividualView> m_parent;
+
+    /**
+     * Maps local indices to indices in parent view. Could be be
+     * optimized to map entire ranges, but for the sake of simplicitly
+     * let's use a 1:1 mapping for now.
+     */
+    std::vector<int> m_local2parent;
+
+ public:
+    /**
+     * Creates an idle IndividualAggregator. Configure it and
+     * subscribe to signals, then call start().
+     */
+    static boost::shared_ptr<FilteredView> create(const boost::shared_ptr<IndividualView> &parent,
+                                                  const boost::shared_ptr<IndividualFilter> &filter);
+
+    /**
+     * Populates view from current content of parent, then
+     * updates it based on incoming signals.
+     */
+    void start();
+
+    /**
+     * Add a FolksIndividual if it matches the filter. Tracking of
+     * changes to individuals is done in parent view.
+     */
+    void addIndividual(int parentIndex, FolksIndividual *individual);
+
+    /**
+     * Removes a FolksIndividual. Might not have been added at all.
+     */
+    void removeIndividual(int parentIndex, FolksIndividual *individual);
+
+    /**
+     * Check whether a changed individual still belongs into the view.
+     */
+    void changeIndividual(int parentIndex, FolksIndividual *individual);
+
+    // from IndividualView
+    virtual int size() const { return (int)m_local2parent.size(); }
+};
+
+/**
+ * Is a normal FolksIndividualAggregator and adds sorting, searching
+ * and browsing to it. At least the full sorted view always exists.
+ */
+class IndividualAggregator
+{
+    boost::shared_ptr<FullView> m_view;
+    boost::weak_ptr<IndividualAggregator> m_self;
+    FolksIndividualAggregatorCXX m_folks;
+
+    IndividualAggregator() {}
+
+    void individualsChanged(GeeSet *added,
+                            GeeSet *removed,
+                            gchar *message) throw ();
+
+ public:
+    /**
+     * Creates an idle IndividualAggregator. Configure it and
+     * subscribe to signals, then call start().
+     */
+    static boost::shared_ptr<IndividualAggregator> create();
+
+    /**
+     * Access to FolksIndividualAggregator which is owned by
+     * the aggregator.
+     */
+    FolksIndividualAggregator *getFolks() const { return m_folks.get(); }
+
+    /**
+     * Starts pulling and sorting of contacts.
+     * Populates m_view and all other, derived views.
+     */
+    void start();
+
+    /** TODO: configure active databases */
+
+    /**
+     * Each aggregator has exactly one full view on the data. This
+     * method grants access to the change signals.
+     */
+    boost::shared_ptr<IndividualView> getMainView() const { return m_view; }
+};
+
 
 SE_END_CXX
 
