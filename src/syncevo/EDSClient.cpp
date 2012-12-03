976c6bd76943d92319a884e0a215a41b34c1ff81
Patrick Ohly <patrick.ohly@intel.com>
2012-12-03 11:37:40 +0100

    EDS: shared loading of ESourceRegistry
    Add a class, EDSRegistryLoader, which owns the creation of
    ESourceRegistry and notifies other components synchronously or
    asynchronously when the instance is ready.

---
+++ src/syncevo/EDSClient.cpp
@@ -0,0 +1,99 @@
+/*
+ * Copyright (C) 2012 Intel Corporation
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) version 3.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301  USA
+ */
+
+#include <syncevo/EDSClient.h>
+
+#include <config.h>
+
+#if defined(HAVE_EDS) && defined(USE_EDS_CLIENT)
+
+#include <boost/bind.hpp>
+
+#include <syncevo/declarations.h>
+SE_BEGIN_CXX
+
+EDSRegistryLoader &EDSRegistryLoader::singleton()
+{
+    static EDSRegistryLoader self;
+    return self;
+}
+
+void EDSRegistryLoader::getESourceRegistryAsync(const Callback_t &cb)
+{
+    singleton().async(cb);
+}
+
+void EDSRegistryLoader::async(const Callback_t &cb)
+{
+    if (m_registry || m_gerror) {
+        cb(m_registry, m_gerror);
+    } else {
+        m_pending.push_back(cb);
+        m_loading = true;
+        SYNCEVO_GLIB_CALL_ASYNC(e_source_registry_new,
+                                boost::bind(&EDSRegistryLoader::created,
+                                            this,
+                                            _1, _2),
+                                NULL);
+    }
+}
+
+ESourceRegistryCXX EDSRegistryLoader::getESourceRegistry()
+{
+    return singleton().sync();
+}
+
+ESourceRegistryCXX EDSRegistryLoader::sync()
+{
+    if (!m_loading) {
+        m_loading = true;
+        SYNCEVO_GLIB_CALL_ASYNC(e_source_registry_new,
+                                boost::bind(&EDSRegistryLoader::created,
+                                            this,
+                                            _1, _2),
+                                NULL);
+    }
+
+    while (true) {
+        if (m_registry) {
+            return m_registry;
+        }
+        if (m_gerror) {
+            m_gerror.throwError("creating source registry");
+        }
+        g_main_context_iteration(NULL, true);
+    }
+}
+
+void EDSRegistryLoader::created(ESourceRegistry *registry, const GError *gerror) throw ()
+{
+    try {
+        m_registry = ESourceRegistryCXX::steal(registry);
+        m_gerror = gerror;
+        BOOST_FOREACH (const Callback_t &cb, m_pending) {
+            cb(m_registry, m_gerror);
+        }
+    } catch (...) {
+        Exception::handle(HANDLE_EXCEPTION_FATAL);
+    }
+}
+
+SE_END_CXX
+
+#endif // HAVE_EDS && USE_EDS_CLIENT
--- src/syncevo/EDSClient.h
+++ src/syncevo/EDSClient.h
@@ -24,15 +24,55 @@
 
 #if defined(HAVE_EDS) && defined(USE_EDS_CLIENT)
 
-#include <syncevo/declarations.h>
 #include <syncevo/GLibSupport.h>
+
+#include <libedataserver/libedataserver.h>
+#include <boost/function.hpp>
+#include <boost/utility.hpp>
+#include <list>
 
 typedef SyncEvo::GListCXX<ESource, GList, SyncEvo::GObjectDestructor> ESourceListCXX;
 SE_GOBJECT_TYPE(ESourceRegistry)
 SE_GOBJECT_TYPE(ESource)
 SE_GOBJECT_TYPE(EClient)
 
-// TODO: create ESourceRegistry on demand and share it inside SyncEvolution.
+#include <syncevo/declarations.h>
+SE_BEGIN_CXX
+
+/**
+ * Creates ESourceRegistry on demand and shares it inside
+ * SyncEvolution. It's never freed once used.
+ */
+class EDSRegistryLoader : private boost::noncopyable
+{
+ public:
+    typedef boost::function<void (const ESourceRegistryCXX &registry,
+                                  const GError *gerror)> Callback_t;
+
+    /**
+     * Callback gets invoked exactly once. If the registry pointer is empty,
+     * then the error will explain why.
+     */
+    static void getESourceRegistryAsync(const Callback_t &cb);
+
+    /**
+     * Returns shared ESourceRegistry, throws error if creation failed.
+     */
+    static ESourceRegistryCXX getESourceRegistry();
+
+ private:
+    Bool m_loading;
+    ESourceRegistryCXX m_registry;
+    GErrorCXX m_gerror;
+    std::list<Callback_t> m_pending;
+
+    static EDSRegistryLoader &singleton();
+    void async(const Callback_t &cb);
+    ESourceRegistryCXX sync();
+    void created(ESourceRegistry *registry, const GError *gerror) throw ();
+};
+
+SE_END_CXX
 
 #endif // HAVE_EDS && USE_EDS_CLIENT
 #endif // INCL_SYNCEVO_EDS_CLIENT
