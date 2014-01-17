e7289857e308c27d779d4b40a222f7190089f09a
Patrick Ohly <patrick.ohly@intel.com>
2014-01-17 05:16:03 -0800

    config: clarify autoSyncInterval
    The interval is measured from the start of last sync. The rationale for that
    is that it makes the time when syncs run independent from the duration of each
    sync, which is more predictable.
    
    The implementation already works like that, it just was not clear from the
    property description.

--- src/syncevo/SyncConfig.cpp
+++ src/syncevo/SyncConfig.cpp
@@ -1775,8 +1775,8 @@ static StringConfigProperty syncPropAutoSync("autoSync",
                                              "0");
 
 static SecondsConfigProperty syncPropAutoSyncInterval("autoSyncInterval",
-                                                      "This is the minimum number of seconds between two\n"
-                                                      "synchronizations that has to pass before starting\n"
+                                                      "This is the minimum number of seconds since the start of\n"
+                                                      "the last synchronization that has to pass before starting\n"
                                                       "an automatic synchronization. Can be specified using\n"
                                                       "a 1h30m5s format.\n"
                                                       "\n"
